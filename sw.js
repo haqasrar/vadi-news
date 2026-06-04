137b2586ae861ed1783502fd9d5d925b43be8cc2
haqasrar <haqasrar264@gmail.com>
2026-06-04 18:36:19 +0530

    Fix PWA icons sizes to meet Chrome requirements

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'vadi-news-cache-v1';
+const CACHE_NAME = 'vadi-news-cache-v1.1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
   '/script.js',
-  '/images/Logo.png'
+  '/images/icon-192.png',
+  '/images/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
